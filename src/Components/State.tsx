152f704190bf03009fb05c06a5881a598bd85c37
awran5 <awran5@yahoo.com>
2022-11-15 02:29:00 +0200

    refactor: modify style

--- src/Components/State.tsx
+++ src/Components/State.tsx
@@ -1,21 +1,27 @@
-import React, { memo } from 'react'
+import React, { memo } from "react";
 
 interface Props {
-  title: string
-  value: number | string
-  symbol?: string
+  title: string;
+  value: number | string;
+  symbol?: string;
 }
 
 function Stats({ title, value, symbol }: Props) {
   return (
-    <div className='box'>
-      <div className='state-title'>{title}</div>
-      <div className='state-value'>
-        <span className='state-value'>{value}</span>
-        {symbol && <span>{symbol}</span>}
+    <div className='col-md col-sm-6'>
+      <div className='card text-center shadow-sm'>
+        <div className='card-body'>
+          <h5 className='text-uppercase text-muted fw-bold mb-2'>{title}</h5>
+          <h2 className='mb-0'>
+            <span className='state-value'>
+              {value}
+              {symbol && symbol}
+            </span>
+          </h2>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default memo(Stats)
+export default memo(Stats);
