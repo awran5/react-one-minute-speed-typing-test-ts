e6c71f82a6c4942794fdec8b16e99a48ceef1ee8
awran5 <awran5@yahoo.com>
2022-11-15 02:55:39 +0200

    feat: add github button script

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,10 +1,20 @@
-import React, { memo } from "react";
+import React, { memo, useEffect } from "react";
 
 type Props = {
   theme: string;
 };
 
 function Footer({ theme }: Props) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://buttons.github.io/buttons.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <footer className={`text-center mt-auto ${theme === "dark" ? "bg-dark" : "bg-light"}`}>
       <div className='container'>
