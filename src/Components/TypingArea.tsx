b9213d24218217214431bda84e32fc5335e2a16a
awran5 <awran5@yahoo.com>
2022-11-15 02:29:44 +0200

    refactor: modify style

--- src/Components/TypingArea.tsx
+++ src/Components/TypingArea.tsx
@@ -1,18 +1,18 @@
-import React, { memo } from 'react'
-import State from './State'
+import React, { memo } from "react";
+import State from "./State";
 
 interface Props {
-  started: boolean
-  incorrect: boolean
-  areaRef: React.RefObject<HTMLDivElement> | null
+  started: boolean;
+  incorrect: boolean;
+  areaRef: React.RefObject<HTMLDivElement> | null;
   // eslint-disable-next-line no-unused-vars
-  handleKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => void
-  remainingText: string
-  completedText: string
-  inputText: string
-  errorIndex: number
-  duration: number
-  handleStart: () => void
+  handleKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => void;
+  remainingText: string;
+  completedText: string;
+  inputText: string;
+  errorIndex: number;
+  duration: number;
+  handleStart: () => void;
 }
 
 function TypingArea({
@@ -20,9 +20,9 @@ function TypingArea({
   incorrect = false,
   areaRef = null,
   handleKeyDown,
-  remainingText = '',
-  completedText = '',
-  inputText = '',
+  remainingText = "",
+  completedText = "",
+  inputText = "",
   errorIndex = 0,
   duration = 0,
   handleStart
@@ -30,7 +30,7 @@ function TypingArea({
   return (
     <>
       <div className='typing-box'>
-        <div className='d-flex text-center justify-content-between align-items-center shadow-sm stats'>
+        <div className='row g-2 mb-4'>
           <State
             title='Progress'
             value={((completedText.length / inputText.length) * 100).toFixed(0)}
@@ -40,8 +40,9 @@ function TypingArea({
           <State title='Timer' value={`${duration}s`} />
         </div>
         <div
-          className={`typing-area${started ? ' active' : ''}${incorrect ? ' is-error' : ''}`}
+          className={`typing-area${started ? " active" : ""}${incorrect ? " is-error" : ""}`}
           ref={areaRef}
+          aria-label='text-box'
           onKeyDown={handleKeyDown}
           tabIndex={0}
           role='textbox'
@@ -50,18 +51,18 @@ function TypingArea({
           <span className='remaining-text'>{remainingText}</span>
         </div>
       </div>
-      <div className='d-grid gap-2 mt-4'>
+      <div className='mt-4'>
         <button
           type='button'
-          className='btn py-3 btn-primary '
+          className='btn py-3 btn-primary w-100 fw-bold text-uppercase'
           disabled={started}
-          onClick={started ? () => {} : handleStart}
+          onClick={started ? undefined : handleStart}
         >
           Start
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default memo(TypingArea)
+export default memo(TypingArea);
