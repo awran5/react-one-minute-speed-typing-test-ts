80820b0a1d444ccddeef0fa6ab0f893f4aef9fe7
awran5 <awran5@yahoo.com>
2022-11-15 02:31:07 +0200

    style: add semi colon

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React, { useRef, useReducer } from 'react'
-import Layout from './Components/Layout'
-import TypingArea from './Components/TypingArea'
-import Result from './Components/Result'
-import { allowedKeys, generateRandomWords } from './helper'
-
-const ErrorBeep = require('./sound/beep.wav')
+import React, { useRef, useReducer } from "react";
+import Layout from "./components/Layout";
+import TypingArea from "./components/TypingArea";
+import Result from "./components/Result";
+import { allowedKeys, generateRandomWords } from "./helper";
+import ErrorBeep from "./sound/beep.wav";
 
 const initialState = {
-  inputText: 'Welcome, click start to begin the one-minute typing speed test',
-  remainingText: 'Welcome, click start to begin the one-minute typing speed test',
-  completedText: '',
+  inputText: "Welcome, click start to begin the one-minute typing speed test",
+  remainingText: "Welcome, click start to begin the one-minute typing speed test",
+  completedText: "",
   showStats: false,
   progress: 0,
   accuracy: 0,
@@ -19,33 +18,33 @@ const initialState = {
   duration: 60,
   incorrect: false,
   started: false
-}
+};
 
-type State = typeof initialState
+type State = typeof initialState;
 
 type Action =
-  | { type: 'START' }
-  | { type: 'END' }
-  | { type: 'DURATION'; payload: number }
-  | { type: 'ACCURACY'; payload: number }
-  | { type: 'WPM'; payload: number }
-  | { type: 'CORRECT' }
-  | { type: 'INCORRECT' }
-  | { type: 'RESET' }
+  | { type: "START" }
+  | { type: "END" }
+  | { type: "DURATION"; payload: number }
+  | { type: "ACCURACY"; payload: number }
+  | { type: "WPM"; payload: number }
+  | { type: "CORRECT" }
+  | { type: "INCORRECT" }
+  | { type: "RESET" };
 
 const reducer = (state: State, action: Action): State => {
-  const randomWords = generateRandomWords()
+  const randomWords = generateRandomWords();
 
   switch (action.type) {
-    case 'START':
+    case "START":
       return {
         ...state,
         started: true,
         showStats: false,
         inputText: randomWords,
         remainingText: randomWords
-      }
-    case 'END':
+      };
+    case "END":
       return {
         ...state,
         started: false,
@@ -53,23 +52,23 @@ const reducer = (state: State, action: Action): State => {
         inputText: state.inputText,
         remainingText: state.remainingText,
         duration: state.duration
-      }
-    case 'DURATION':
+      };
+    case "DURATION":
       return {
         ...state,
         duration: action.payload
-      }
-    case 'ACCURACY':
+      };
+    case "ACCURACY":
       return {
         ...state,
         accuracy: action.payload
-      }
-    case 'WPM':
+      };
+    case "WPM":
       return {
         ...state,
         wpm: action.payload
-      }
-    case 'CORRECT':
+      };
+    case "CORRECT":
       return {
         ...state,
         progress: state.progress + 1,
@@ -77,20 +76,20 @@ const reducer = (state: State, action: Action): State => {
         remainingText: state.remainingText.slice(1),
         completedText: state.completedText + state.remainingText.charAt(0),
         incorrect: false
-      }
-    case 'INCORRECT':
+      };
+    case "INCORRECT":
       return {
         ...state,
         incorrect: true,
         errorIndex: state.errorIndex + 1
-      }
-    case 'RESET':
-      return initialState
+      };
+    case "RESET":
+      return initialState;
 
     default:
-      return state
+      return state;
   }
-}
+};
 
 function App() {
   const [
@@ -109,64 +108,64 @@ function App() {
       started
     },
     dispatch
-  ] = useReducer(reducer, initialState)
+  ] = useReducer(reducer, initialState);
 
-  const interval = useRef<number | null>(null)
-  const areaRef = useRef<HTMLDivElement | null>(null)
-  const errorBeepRef = useRef<HTMLAudioElement | null>(null)
+  const interval = useRef<number | null>(null);
+  const areaRef = useRef<HTMLDivElement | null>(null);
+  const errorBeepRef = useRef<HTMLAudioElement | null>(null);
 
   const handleStart = () => {
-    dispatch({ type: 'START' })
-    if (areaRef.current) areaRef.current.focus()
+    dispatch({ type: "START" });
+    if (areaRef.current) areaRef.current.focus();
 
     // Timer
-    const now = Date.now()
-    const seconds = now + 60 * 1000
+    const now = Date.now();
+    const seconds = now + 60 * 1000;
 
     interval.current = window.setInterval(() => {
-      const secondLeft = Math.round((seconds - Date.now()) / 1000)
+      const secondLeft = Math.round((seconds - Date.now()) / 1000);
       if (secondLeft <= 0) {
-        dispatch({ type: 'END' })
-        if (interval.current) window.clearInterval(interval.current)
+        dispatch({ type: "END" });
+        if (interval.current) window.clearInterval(interval.current);
       }
-      dispatch({ type: 'DURATION', payload: secondLeft })
-    }, 1000)
-  }
+      dispatch({ type: "DURATION", payload: secondLeft });
+    }, 1000);
+  };
 
   const handleKeyDown = (e: React.KeyboardEvent<Element>) => {
-    if (showStats || !started || duration === 0) return
+    if (showStats || !started || duration === 0) return;
 
-    e.preventDefault()
-    const { key } = e
+    e.preventDefault();
+    const { key } = e;
 
-    if (allowedKeys.includes(key) && key !== 'Shift') {
+    if (allowedKeys.includes(key) && key !== "Shift") {
       if (key === inputText.charAt(progress)) {
-        dispatch({ type: 'CORRECT' })
+        dispatch({ type: "CORRECT" });
       } else {
-        dispatch({ type: 'INCORRECT' })
+        dispatch({ type: "INCORRECT" });
 
         // error sound
         if (errorBeepRef.current) {
-          errorBeepRef.current.currentTime = 0
-          errorBeepRef.current.play()
+          errorBeepRef.current.currentTime = 0;
+          errorBeepRef.current.play();
         }
       }
       if (progress > 5)
         dispatch({
-          type: 'ACCURACY',
+          type: "ACCURACY",
           payload: Math.floor(((progress - errorIndex) / progress) * 100)
-        })
+        });
 
-      if (progress + 1 === inputText.length || errorIndex >= 50) dispatch({ type: 'END' })
+      if (progress + 1 === inputText.length || errorIndex >= 50) dispatch({ type: "END" });
       // set wpm
-      dispatch({ type: 'WPM', payload: Math.round(correctIndex / 5) })
+      dispatch({ type: "WPM", payload: Math.round(correctIndex / 5) });
     }
-  }
+  };
 
   const handleReload = () => {
-    dispatch({ type: 'RESET' })
-    setTimeout(() => handleStart(), 0)
-  }
+    dispatch({ type: "RESET" });
+    setTimeout(() => handleStart(), 100);
+  };
 
   return (
     <>
@@ -197,9 +196,9 @@ function App() {
         )}
       </Layout>
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <audio src={ErrorBeep.default} ref={errorBeepRef} hidden />
+      <audio src={ErrorBeep} ref={errorBeepRef} hidden />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
